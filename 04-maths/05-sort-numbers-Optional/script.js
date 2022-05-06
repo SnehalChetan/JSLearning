39ce34dca4352241d07cd8e65c6331a0b877c139
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-05-06 15:20:09 +0200

    display numbers in a given string in ascending order

--- 04-maths/05-sort-numbers-Optional/script.js
+++ 04-maths/05-sort-numbers-Optional/script.js
@@ -16,7 +16,35 @@
     document.getElementById("run").addEventListener("click", function() {
 
         // your code here
+        const givenStr = document.getElementById("numbers").value;
+        let temp = 0;
+        let strArray = givenStr.split(','); //convert string to array
+                    //console.log("Num Array = "+strArray);//2, 4, 14, 10, 90, 23, 16
+                    //let strArray=[2,4,14,10,90,16];
+                    //console.log("arr Length = "+strArray.length);
 
+        let numArray = new Array();
+        for(let i=0; i < strArray.length; i++){
+            numArray.push(parseInt(strArray[i]));
+        }
+        console.log("Num Array = "+numArray);
+
+        for(let i=0; i<=(numArray.length-1); i++){
+
+            for(let j=i+1; j < numArray.length; j++){
+                console.log("i = "+i+"  j= "+j);
+                console.log("temp = "+temp+" numArray["+i+"] = "+numArray[i]+" numArray["+j+"] = "+numArray[j]);
+                if(numArray[i] > numArray[j]){ console.log("Inside if");
+                    temp = numArray[i];
+                    numArray[i] = numArray[j];
+                    numArray[j] = temp;
+                    /*console.log("After Conversion");
+                    console.log("temp = "+temp+" numArray["+i+"] = "+numArray[i]+" numArray["+j+"] = "+numArray[j]);*/}
+               // console.log("\n");
+            }
+           // console.log("new Array2 = "+numArray);
+        }
+        document.getElementById("result").innerHTML="The Sorted Array = "+numArray;
     });
 
 })();
