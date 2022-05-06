9cc20ee39a17641f367384e6fbd6b4dc35f45a97
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-05-06 11:44:50 +0200

    display months of spookey friday of selected year

--- 03-dates/05-get-spooky-fridays-Optional/script.js
+++ 03-dates/05-get-spooky-fridays-Optional/script.js
@@ -12,5 +12,31 @@
 (function() {
 
     // your code here
+    const months    = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const runBtn = document.getElementById("run");
+    const date = new Date();  //console.log("date= "+date.getDate()+" day= "+date.getDay()+" year= "+date.getFullYear());
+    const spookyDate = 13;
+    const spookyMonth = new Array();
+    runBtn.onclick=function(){
+        const year = document.getElementById("year").value; // 2000 
+        date.setFullYear(year); //console.log("year = "+date.getFullYear()); //2000
+        date.setDate(spookyDate); //console.log("day  = "+date.getDate()); 
+        months.forEach((month, index, months)=>{
+           
+
+            date.setMonth(index); //console.log("set month = "+date.getMonth());
+            if(date.getDay()==5){
+                    /*console.log("spooky day = "+date.getDate()+","+weekDays[date.getDay()]+"Date = "+date);*/
+                    spookyMonth.push(month);
+            }
+            console.log("spookeyMonths = "+spookyMonth);
+            document.getElementById("givenyear").innerHTML = "The spookey months of the year "+date.getFullYear();
+            document.getElementById("result").innerHTML = spookyMonth.join();
+        });
+    }   
+
+
 
 })();
