f69a48a56b577090db32f76a9555c60f78918a87
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-05-06 12:08:51 +0200

    display message if number divisible of 3 or 5 or both or none

--- 04-maths/03-fizzbuzz/script.js
+++ 04-maths/03-fizzbuzz/script.js
@@ -12,5 +12,17 @@
 (function() {
 
     // your code here
-
+    //In the console, for each numbers between 1 and 100 : show "fizz" if the number is a multiple of 3, "buzz" if it's a multiple of 5, "fizzbuzz" if it's both, or simply the number if it's none of these.
+    for(let i=0;i<=100;i++){
+        
+        if(i%3==0){
+            console.log("fizz");
+        } else if(i%5==0){
+            console.log("buzz");
+        }else if(i%3==0 && i%5==0){
+            console.log("fizzbuzz");
+        } else{
+            console.log(i)
+        }
+    }
 })();
