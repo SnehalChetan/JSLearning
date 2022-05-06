8cbd4e4acc184291bc4e73638c7ea537ba8c3cb3
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-05-06 09:32:24 +0200

    confirm box Exercise- ask and confirm user information  and if wrong then re-run the process

--- 01-base/03-prompt-if/script.js
+++ 01-base/03-prompt-if/script.js
@@ -11,6 +11,21 @@
 
 (function() {
 
-  // your code here
+  let uname = prompt("Enter your name : ");
+    let ugender = prompt("Enter your Gender : ");
+    let ucity = prompt("Where do you live : ");
+    if(uname !='' && ugender !=''&&ucity !=''){
+        let udata = confirm("Does your information correct?"+ "\nName : "+uname+"\nGender : "+ugender+"\nCity : "+ucity);
+        if( udata == true ){
+            alert(" Thank you!"); 
+        }
+        else{
+            alert("Please refill the information");
+            location.reload();
+        }
+    }else{
+        alert("Please refill all the information");
+        location.reload();
+    }
 
 })();
