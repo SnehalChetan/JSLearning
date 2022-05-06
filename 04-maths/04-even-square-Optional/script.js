7aaa925d669d664cc7515ed8f6d2220d7c6022ea
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-05-06 12:27:39 +0200

    display numbers which are square root of the numbers between 1 to 21

--- 04-maths/04-even-square-Optional/script.js
+++ 04-maths/04-even-square-Optional/script.js
@@ -12,9 +12,16 @@
 (function() {
 
     document.getElementById("run").addEventListener("click", function() {
-
+        const sqrtNum = new Array();
         // your code here
-
+        for(let i=1; i<=21; i++){
+            let num = Math.sqrt(i); //may return the floating point number e.g: Math.sqrt(3)=1.73
+            let val = num% 1 !== 0; // it gives original sqrt numbers
+            if(!val){
+                sqrtNum.push(i);
+            }
+        }
+        document.getElementById("result").innerHTML=sqrtNum.join();
     });
 
 })();
