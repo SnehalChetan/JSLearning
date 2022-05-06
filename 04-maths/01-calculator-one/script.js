19df3b728fbccab8301b111c280dd06e673e0b41
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-05-06 12:06:24 +0200

    simple calculator using button click event

--- 04-maths/01-calculator-one/script.js
+++ 04-maths/01-calculator-one/script.js
@@ -14,17 +14,41 @@
 
     document.getElementById("addition").addEventListener("click", function() {
         // perform an addition
+        const val1 =parseInt(document.getElementById("op-one").value);
+        const val2 =parseInt(document.getElementById("op-two").value);
+        console.log("val1 =".val1); 
+        console.log("val2 =".val2);
+        let res = val1 + val2;
+        console.log("Add ="+res);
+        document.getElementById("result").innerHTML = res;
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
         // perform an substraction
+        const val1 =parseInt(document.getElementById("op-one").value);
+        const val2 =parseInt(document.getElementById("op-two").value);
+        console.log("val1 =".val1); console.log("val2 =".val2);
+        let res = val1-val2;
+        console.log("sub ="+res);
+        document.getElementById("result").innerHTML = res;
+
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
         // perform an multiplication
+        const val1 =document.getElementById("op-one").value;
+        const val2 =document.getElementById("op-two").value;
+        console.log("val1 =".val1); console.log("val2 =".val2);
+        let res = val1*val2;console.log("multi ="+res);
+        document.getElementById("result").innerHTML = res;
     });
 
     document.getElementById("division").addEventListener("click", function() {
         // perform an division
+        const val1 =document.getElementById("op-one").value;
+        const val2 =document.getElementById("op-two").value;
+        console.log("val1 =".val1); console.log("val2 =".val2);
+        let res = val1/val2;console.log("Div ="+res);
+        document.getElementById("result").innerHTML = res;
     });
 })();
