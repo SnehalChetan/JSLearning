7558183af92a6a4b9d15eba473de2426808aa0f9
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-05-06 09:57:56 +0200

    display todays date in proper format

--- 03-dates/04-locale-date-Optional/script.js
+++ 03-dates/04-locale-date-Optional/script.js
@@ -13,6 +13,21 @@
 
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
-    // your code here
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const currentDateTime = new Date();
+    
+    const day = weekDays[currentDateTime.getDay()];
+    const todaysDate = currentDateTime.getDate();
+    const month = months[currentDateTime.getMonth()];
+    const thisYear = currentDateTime.getFullYear();
+    const currentHour = currentDateTime.getHours();
+    const currentMinutes = currentDateTime.getMinutes();
+ 
+    const today = day+" "+todaysDate+" "+month+" "+thisYear+","+" "+currentHour+"h"+currentMinutes;
+    document.getElementById("target").innerHTML = today;
+    
+    
 
 })();
