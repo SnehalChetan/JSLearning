122d181835df8b242237bfd4b415058467c693d6
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-05-10 10:38:02 +0200

    apply password check regex and cahnge color of input box on false condition

--- 06-dom/08-match-password-one/script.js
+++ 06-dom/08-match-password-one/script.js
@@ -10,7 +10,43 @@
 // You will have time to focus on it later.
 
 (function() {
+/**
+ * password must be 8-15 charachters
+ * contains atleast one special character /['~!@#$%&*_+=]/
+ * one capital letter /[a-zA-Z]/
+ * atlest one number  /\d/
+ * 
+ */
+ 
 
     // your code here
+    document.getElementById("run").addEventListener("click",()=>{
+        const pwd1 = document.getElementById("pass-one");
+        const pwd2 = document.getElementById("pass-two");
+
+        pwd1Val = String(pwd1.value);
+        pwd2Val = String(pwd2.value);
+       
+        if(pwd1Val.match(/\d/) && pwd1Val.match(/[a-zA-Z]/) && pwd1Val.match(/['~!@#$%&*_+=]/)){
+            if(pwd1Val.length >= 8 && pwd1Val != '') {
+                if(pwd1Val == pwd2Val && pwd1Val.length == pwd2Val.length){
+                    console.log("password matching "+pwd1);
+                    pwd1.style.borderColor="green";
+                    pwd2.style.borderColor="green";
+                }else{
+                    pwd1.style.borderColor="red";
+                    pwd2.style.borderColor="red";
+                    console.log("Please enter similar password in both fields");
+                }
+            }else{ console.log("password must not be empty");
+                pwd1.style.borderColor="red";
+                pwd2.style.borderColor="red";
+            }
+        } else{
+            pwd1.style.borderColor="red";
+            pwd2.style.borderColor="red";
+            alert("invalid password.Match all cases"+"\n 1. password must be 8-15 charachter long \n 2. Contains atleast one special character \n 3. One capital letter \n4. atlest one number");
+        }
+});
 
 })();
--- 06-dom/09-match-password-two-Optional/script.js
+++ 06-dom/09-match-password-two-Optional/script.js
@@ -12,5 +12,35 @@
 (function() {
 
     // your code here
+    
+    // your code here
+    document.getElementById("run").addEventListener("click",()=>{
+        const pwd1 = document.getElementById("pass-one");
+        const pwd2 = document.getElementById("pass-two");
+
+        pwd1Val = String(pwd1.value);
+        pwd2Val = String(pwd2.value);
+       
+        if(pwd1Val.match(/\d/) && pwd1Val.match(/[a-zA-Z]/) && pwd1Val.match(/['~!@#$%&*_+=]/)){
+            if(pwd1Val.length >= 8 && pwd1Val != '') {
+                if(pwd1Val == pwd2Val && pwd1Val.length == pwd2Val.length){
+                    console.log("password matching "+pwd1);
+                    pwd1.style.borderColor="green";
+                    pwd2.style.borderColor="green";
+                }else{
+                    pwd1.className="error";
+            pwd2.className="error";
+                    console.log("Please enter similar password in both fields");
+                }
+            }else{ console.log("password must not be empty");
+                pwd1.className="error";
+                pwd2.className="error";
+            }
+        } else{
+            pwd1.className="error";
+            pwd2.className="error";
+            alert("invalid password.Match all cases"+"\n 1. password must be 8-15 charachter long \n 2. Contains atleast one special character \n 3. One capital letter \n4. atlest one number");
+        }
+});
 
 })();
