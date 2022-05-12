32dade7134d881de774ea6902a53605c28f94a29
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-05-12 14:35:45 +0200

    display data from fetch function to html template by cloning html data and append it

--- 07-fetch/02-details/script.js
+++ 07-fetch/02-details/script.js
@@ -1,6 +1,6 @@
 /* becode/javascript
  *
- * /09-fetch/03-details/script.js - 11.3: details
+ * /09-fetch/02-list-to-template/script.js - 11.2: liste vers template
  *
  * coded by leny@BeCode
  * started at 12/05/2019
@@ -11,4 +11,39 @@
 
 (() => {
     // your code here
+    const url = "../../_shared/api.json";
+    document.querySelector("#run").addEventListener("click",()=>{
+        let getHero  = document.querySelector("#hero-id").value;
+        displayHeroData(getHero,url);
+   
+
+    async function displayHeroData(getHero,url){
+        const responseHero = await getHeroData(url);
+        let tmplHero = document.getElementById("tpl-hero");
+        let cloneTmpl = tmplHero.content.cloneNode(true);// console.log(responseHero);
+        const heroArr = responseHero['heroes'];//console.log("length ="+heroArr.length);console.log(heroArr);
+        let nametag = cloneTmpl.querySelector('.name');
+       // nametag.innerText = "hello";
+        let egotag = cloneTmpl.querySelector('.alter-ego');
+        let powertag = cloneTmpl.querySelector(".powers");
+
+
+         heroArr.forEach(hero => {
+                //console.log(hero);
+                if(hero['id']==getHero){
+                    console.log(hero['id']+" "+hero['name']+" "+hero['alterEgo']+" "+hero['abilities']);
+                    nametag.innerText = hero['name'];
+                    egotag.innerText = hero['alterEgo'];
+                    powertag.innerText = hero['abilities'];
+                    
+                }
+            });/**/
+        document.getElementById('target').appendChild(cloneTmpl);
+    } 
+    async function getHeroData(url){
+        let heroData = await fetch(url);
+        let heroJson = heroData.json();
+        return heroJson;
+    }
+});
 })();
