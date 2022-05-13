69b33f2cbf06b3b96da57520518542393691d8c6
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-05-13 14:13:20 +0200

    delete the specific element from array fetch from api

--- 07-fetch/05-delete-Optional/script.js
+++ 07-fetch/05-delete-Optional/script.js
@@ -11,4 +11,32 @@
 
 (() => {
     // your code here
+    const url = "../../_shared/api.json";
+    var arrayHero = new Array();
+    (async ()=>{   
+        try {
+            const fetchResponse = await fetch(`../../_shared/api.json`);
+            const data = await fetchResponse.json();
+            arrayHero = data.heroes;
+            console.log(arrayHero);
+        } catch (e) {
+            console.log("error");
+            console.log(e);
+        } 
+    })();
+
+    
+    document.getElementById("run").addEventListener("click",()=>{
+        let removeHero = document.getElementById("hero-id").value;
+        let found = arrayHero.find(hero => hero.id == removeHero ); // find() returns complete data of specific id
+       
+       if(found){
+            arrayHero.splice(arrayHero.indexOf(found),1); 
+            console.log(arrayHero);
+        } else{
+           alert("Hero not available, please check for another hero");
+        } 
+        
+
+    });
 })();
